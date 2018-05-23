56e3aac7e4ec6c4f1571c277a692f1160a5967e4
prakashdwd <prakash.d.aux@go-jek.com>
2018-05-23 15:58:19 +0530

    create post and it's test

--- test/integration/post_flows_test.rb
+++ test/integration/post_flows_test.rb
@@ -2,7 +2,18 @@ require 'test_helper'
 
 class PostFlowsTest < ActionDispatch::IntegrationTest
   test "It should load post#index" do
-	  get "/"
-	  assert_response :success
+	get "/"
+	assert_select "h1" , "Blog Posts"
   end
+
+  test "Should create Post" do	
+  	get "/posts/new"
+  	assert_response :success
+  	post "/posts" , params: {post: {title: "Post Title" , body: "Post Body"}}
+  	assert_response :redirect
+  	follow_redirect!
+  	assert_response :success
+  	assert_select "h2" , "Post Title"
+  end
+
 end
