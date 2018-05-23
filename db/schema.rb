80b67a52c9a3c16d447a881b640f40e2874072ae
prakashdwd <prakash.d.aux@go-jek.com>
2018-05-23 19:30:49 +0530

    New table for categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_23_065316) do
+ActiveRecord::Schema.define(version: 2018_05_23_135040) do
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
